ee8396e5a010ebc285c03ea6a14b8be0dde77509
ViktoriaErokhina <viktoria.erokhina@gmail.com>
2018-04-27 14:55:42 +0300

    add delivery options

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -2,9 +2,9 @@ import React from 'react';
 import * as UI from '@vkontakte/vkui';
 import '@vkontakte/vkui/dist/vkui.css';
 import '../style/cart.css';
-import {setActivePanel, setCategory, setItemData} from "../redux/actions";
-import {connect} from "react-redux";
-import {parse} from "../lib/yandex";
+import {setActivePanel, setCategory, setItemData} from '../redux/actions';
+import {connect} from 'react-redux';
+import {parse} from '../lib/yandex';
 
 const mapStateToProps = state => {
     return {
@@ -103,7 +103,7 @@ class ConnectedCart extends React.Component {
                         }
                         {/*<UI.ListItem>*/}
                             {/*<div className='cart-item'>*/}
-                                {/*<img className='cart-item-image' src="https://content.nike.com/content/dam/one-nike/en_us/Jordan/sp18/slp/desktop/0328-jordan-slp-p4-iridescent.jpg.transform/full-screen/0328-jordan-slp-p4-iridescent.jpg"/>*/}
+                                {/*<img className='cart-item-image' src='https://content.nike.com/content/dam/one-nike/en_us/Jordan/sp18/slp/desktop/0328-jordan-slp-p4-iridescent.jpg.transform/full-screen/0328-jordan-slp-p4-iridescent.jpg'/>*/}
                                 {/*<div className='cart-item-info'>*/}
                                     {/*<div className='cart-item-name'>Nike Air Max 270 Flyknit</div>*/}
                                     {/*<div className='cart-item-options'>*/}
@@ -131,8 +131,95 @@ class ConnectedCart extends React.Component {
                     </div>
                 </UI.Group>
 
+                <UI.Group>
+                    <UI.Pane>
+                        <div className='cart-group-title'>Оформление заказа</div>
+                        <div className='iteminfo-option'>Имя и фамилия</div>
+                        <textarea className='cart-textarea'/>
+                        <div className='iteminfo-option'>Мобильный телефон</div>
+                        <textarea className='cart-textarea'/>
+                    </UI.Pane>
+                    <UI.Pane>
+                        <div className='cart-group-title'>Доставка</div>
+                        <div className='iteminfo-option'>Способ доставки</div>
+                        <div className='iteminfo-select-container'>
+                            <select className='iteminfo-select'>
+                                <option className='iteminfo-select-option'>Экспресс-доставка</option>
+                            </select>
+                        </div>
+                        <div className='iteminfo-option'>Адрес доставки</div>
+                        <textarea className='cart-textarea'/>
+                        <div className='iteminfo-option'>Почтовый индекс</div>
+                        <textarea className='cart-textarea'/>
+                    </UI.Pane>
+                    <UI.Pane>
+                        <div className='cart-group-title'>Оплата</div>
+                        <div className='iteminfo-option'>Способ оплаты</div>
+                        <div className='iteminfo-select-container'>
+                            <select className='iteminfo-select'>
+                                <option className='iteminfo-select-option'>VK Pay</option>
+                            </select>
+                        </div>
+                        {/*<div className='checkbox-container'>*/}
+                            {/*<input type='checkbox' className='checkbox' checked='checked' />*/}
+                            {/*<div className='checkbox-text'>У меня есть промокод на скидку</div>*/}
+                        {/*</div>*/}
+
+                        {/*<label className="container">One*/}
+                            {/*<input type="checkbox" checked="checked" />*/}
+                                {/*<span className="checkmark"/>*/}
+                        {/*</label>*/}
+                        <div className='checkbox-wrapper'>
+                            <UI.List>
+                                <UI.ListItem asideContent={<UI.Switch />}>
+                                    <div className='checkbox-text'>У меня есть промокод на скидку</div>
+                                </UI.ListItem>
+                            </UI.List>
+                        </div>
+                        {/*<div className='checkbox-container'>*/}
+                            {/*<div className='checkbox-text-container'>*/}
+                                {/*<div className='checkbox-text'>У меня есть промокод на скидку</div>*/}
+                            {/*</div>*/}
+                            {/*<UI.Switch className='checkbox'/>*/}
+                        {/*</div>*/}
+                    </UI.Pane>
+                    {/*<UI.Header className='group-header' level='1'>*/}
+                        {/*<div className='group-title'>*/}
+                            {/*Оформление заказа*/}
+                        {/*</div>*/}
+                    {/*</UI.Header>*/}
+                    {/*<UI.List>*/}
+                        {/*<div className='iteminfo-option'>Имя и фамилия</div>*/}
+                        {/*<div className='iteminfo-select-container'>*/}
+                            {/*<select className='iteminfo-select'>*/}
+                                {/*<option className='iteminfo-select-option'>US 8 (RU 40)</option>*/}
+                            {/*</select>*/}
+                        {/*</div>*/}
+                        {/*<div className='iteminfo-option'>Мобильный телефон</div>*/}
+                        {/*<div className='iteminfo-select-container'>*/}
+                            {/*<select className='iteminfo-select'>*/}
+                                {/*<option className='iteminfo-select-option'>US 8 (RU 40)</option>*/}
+                            {/*</select>*/}
+                        {/*</div>*/}
+                    {/*</UI.List>*/}
+                {/*</UI.Group>*/}
+                {/*<UI.Group>*/}
+                    {/*<UI.Header className='group-header' level='1'>*/}
+                        {/*<div className='group-title'>*/}
+                            {/*Доставка*/}
+                        {/*</div>*/}
+                    {/*</UI.Header>*/}
+                {/*</UI.Group>*/}
+                {/*<UI.Group>*/}
+                    {/*<UI.Header className='group-header' level='1'>*/}
+                        {/*<div className='group-title'>*/}
+                            {/*Оплата*/}
+                        {/*</div>*/}
+                    {/*</UI.Header>*/}
+                </UI.Group>
+
                 <div id='cart-button-item' className='cart-button-container-item'>
-                    <UI.Button appearance="vk-rich" className='cart-button-item'
+                    <UI.Button appearance='vk-rich' className='cart-button-item'
                                onClick={() => {
                                    this.openNotifyDialog();
                                }}>Оформить заказ</UI.Button>
